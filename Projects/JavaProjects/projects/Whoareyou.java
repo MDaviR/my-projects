c170612a6751dc5f6df612eaa9bd0b21edc5c0ce
MDaviR <121315239+MDaviR@users.noreply.github.com>
2023-01-03 14:27:29 -0300

    Update and rename whoareyou.java to Whoareyou.java

--- Projects/JavaProjects/projects/Whoareyou.java
+++ Projects/JavaProjects/projects/Whoareyou.java
@@ -1,6 +1,6 @@
 import java.util.Locale;
 import java.util.Scanner; 
-public class whoareyou { 
+public class Whoareyou { 
 public static void main(String[] args) {
 
  Locale.setDefault(Locale.US);
