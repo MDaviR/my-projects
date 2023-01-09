cc2e4ba569fca98d4f7bc3ab0e15bd9211452834
MDaviR <121315239+MDaviR@users.noreply.github.com>
2023-01-09 14:43:37 -0300

    Update ContaPoupanca.java

--- Projects/JavaProjects/projects/ExerciciosProjetosDeCursosEFaculdade/Bank system/Herança,Override,Polimorfismo,SuperEConstrutor/src/ContaPoupanca.java
+++ Projects/JavaProjects/projects/ExerciciosProjetosDeCursosEFaculdade/Bank system/Herança,Override,Polimorfismo,SuperEConstrutor/src/ContaPoupanca.java
@@ -1,5 +1,5 @@
 public class ContaPoupanca extends Conta {
-    public ContaPoupanca(int agencia, int numero){//Re utilizando o construtor da classe Conta.
+    public ContaPoupanca(int agencia, int numero){//Re-utilizando o construtor da classe Conta.
         super(agencia, numero);//Invocando o construtor.
     }
     
