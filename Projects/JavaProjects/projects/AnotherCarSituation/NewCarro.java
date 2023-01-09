6de926df5ff9075af67d100dabe1abcd1a68d3e4
MDaviR <121315239+MDaviR@users.noreply.github.com>
2023-01-09 18:57:33 -0300

    Update NewCarro.java

--- Projects/JavaProjects/projects/AnotherCarSituation/NewCarro.java
+++ Projects/JavaProjects/projects/AnotherCarSituation/NewCarro.java
@@ -1,7 +1,7 @@
 package AnotherCarSituation;
 public class NewCarro {
 	public static void main(String[] args) {
-		Carro carro = new Carro(1900, "Fusca", 35000);
+		Carro carro1 = new Carro(1900, "Fusca", 35000);
 		Carro carro2 = new Carro(1901, "BMW", 50000);
 		Carro carro3 = new Carro(1902, "HighLux", 200000);
 		
@@ -12,10 +12,10 @@ public class NewCarro {
 		System.out.println();
 		
 		System.out.println("==================================================");
-		System.out.println("* Modelo do veiculo: "+carro.getModelo());
-		System.out.println("- Valor do carro: "+carro.getPreco());
-		System.out.println("* Ano do carro: "+carro.getAno());
-		System.out.println("- Localização na memória "+carro);
+		System.out.println("* Modelo do veiculo: "+carro1.getModelo());
+		System.out.println("- Valor do carro: "+carro1.getPreco());
+		System.out.println("* Ano do carro: "+carro1.getAno());
+		System.out.println("- Localização na memória "+carro1);
 		System.out.println("==================================================");
 		
 		System.out.println();
